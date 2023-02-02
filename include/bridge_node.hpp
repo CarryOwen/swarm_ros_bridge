8f3810997b3afb13a7230f42beded9df9c806f43
CarryOwen <ksyxt0700379@163.com>
2023-02-02 09:54:11 +0000

    zmqpp版本也可以用cppzmq接收

--- include/bridge_node.hpp
+++ include/bridge_node.hpp
@@ -25,8 +25,6 @@
 #include <unistd.h>
 #include <string>
 #include <zmqpp/zmqpp.hpp>
-#include "zmq2.hpp"
-#include "zmq_addon.hpp"
 /*
 zmqpp is the c++ wrapper around ZeroMQ
 Intall zmqpp first:
@@ -58,12 +56,9 @@ std::vector<TopicInfo> sendTopics; // send topics info struct vector
 std::vector<TopicInfo> recvTopics; // receive topics info struct vector
 
 // ********************* zmq socket initialize ***************************
-// zmqpp::context_t context;
-// std::vector<std::unique_ptr<zmqpp::socket>> senders;   //index senders
-// std::vector<std::unique_ptr<zmqpp::socket>> receivers; //index receivers
-zmq::context_t context;
-std::vector<std::unique_ptr<zmq::socket_t>> senders;   // index senders
-std::vector<std::unique_ptr<zmq::socket_t>> receivers; // index receivers
+zmqpp::context_t context;
+std::vector<std::unique_ptr<zmqpp::socket>> senders;   //index senders
+std::vector<std::unique_ptr<zmqpp::socket>> receivers; //index receivers
 
 // ******************* ROS subscribe and publish *************************
 std::vector<ros::Time> sub_t_last;
